e95ad2cb66646a72dbce31dea28e9b073313aed7
akhilmantha <akhilmantha@live.com>
2018-09-08 10:30:08 +0530

    changes in coredata stack

--- VirtualTourist/CoreDataStack.swift
+++ VirtualTourist/CoreDataStack.swift
@@ -2,8 +2,92 @@
 //  CoreDataStack.swift
 //  VirtualTourist
 //
-//  Created by akhil mantha on 08/09/18.
+//  Created by akhil mantha on  23/08/18.
 //  Copyright © 2018 akhil mantha. All rights reserved.
 //
 
-import Foundation
+import CoreData
+
+// MARK: - CoreDataStack
+
+struct CoreDataStack {
+    
+    // MARK: Properties
+    
+    private let model: NSManagedObjectModel
+    internal let coordinator: NSPersistentStoreCoordinator
+    private let modelURL: URL
+    internal let dbURL: URL
+    let context: NSManagedObjectContext
+    
+    // MARK: Initializers
+    
+    init?(modelName: String) {
+        
+        // Assumes the model is in the main bundle
+        guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd") else {
+            print("Unable to find \(modelName)in the main bundle")
+            return nil
+        }
+        self.modelURL = modelURL
+        
+        guard let model = NSManagedObjectModel(contentsOf: modelURL) else {
+            print("unable to create a model from \(modelURL)")
+            return nil
+        }
+        self.model = model
+        
+        coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+        
+        context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        context.persistentStoreCoordinator = coordinator
+        
+        let fm = FileManager.default
+        
+        guard let docUrl = fm.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("Unable to reach the documents folder")
+            return nil
+        }
+        
+        self.dbURL = docUrl.appendingPathComponent("model.sqlite")
+        
+        print(self.dbURL)
+        
+        // Options for migration
+        let options = [NSInferMappingModelAutomaticallyOption: true,NSMigratePersistentStoresAutomaticallyOption: true]
+        
+        do {
+            try addStoreCoordinator(NSSQLiteStoreType, configuration: nil, storeURL: dbURL, options: options as [NSObject : AnyObject]?)
+        } catch {
+            print("unable to add store at \(dbURL)")
+        }
+    }
+    
+    // MARK: Utils
+    
+    func addStoreCoordinator(_ storeType: String, configuration: String?, storeURL: URL, options : [NSObject:AnyObject]?) throws {
+        try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: dbURL, options: nil)
+    }
+}
+
+
+internal extension CoreDataStack  {
+    
+    func dropAllData() throws {
+        // delete all the objects in the db. This won't delete the files, it will
+        // just leave empty tables.
+        try coordinator.destroyPersistentStore(at: dbURL, ofType:NSSQLiteStoreType , options: nil)
+        try addStoreCoordinator(NSSQLiteStoreType, configuration: nil, storeURL: dbURL, options: nil)
+    }
+}
+
+// MARK: - CoreDataStack (Save Data)
+
+extension CoreDataStack {
+    
+    func saveContext() throws {
+        if context.hasChanges {
+            try context.save()
+        }
+    }
+}
