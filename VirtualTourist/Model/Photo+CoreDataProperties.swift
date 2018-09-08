53dc464277718fd41c53b3b5af0c5bf34170b151
akhilmantha <akhilmantha@live.com>
2018-09-08 09:43:03 +0530

    refactoring-properties&model class

---
+++ VirtualTourist/Model/Photo+CoreDataProperties.swift
@@ -0,0 +1,9 @@
+//
+//  Photo+CoreDataProperties.swift
+//  VirtualTourist
+//
+//  Created by akhil mantha on 23/08/18.
+//  Copyright © 2018 akhil mantha. All rights reserved.
+//
+
+import Foundation
---
+++ VirtualTourist/Model/Pin+CoreDataProperties.swift
@@ -0,0 +1,9 @@
+//
+//  Pin+CoreDataProperties.swift
+//  VirtualTourist
+//
+//  Created by akhil mantha on 23/08/18.
+//  Copyright © 2018 akhil mantha. All rights reserved.
+//
+
+import Foundation
