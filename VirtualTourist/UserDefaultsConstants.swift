d3644b0880703546f0ff824627d110c27b3df711
akhilmantha <akhilmantha@live.com>
2018-09-08 10:37:16 +0530

    inverse relationship for data model entity

---
+++ VirtualTourist/UserDefaultsConstants.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsConstants.swift
+//  VirtualTourist
+//
+//  Created by akhil mantha on 08/09/18.
+//  Copyright © 2018 akhil mantha. All rights reserved.
+//
+
+import Foundation
+
+struct UserDefaultsConstants {
+    static let MapViewCenterLatitudeKey = "MapViewCenterLatitudeKey"
+    static let MapViewCenterLongitudeKey = "MapViewCenterLongitudeKey"
+    static let MapViewRegionLatitudeDeltaKey = "MapViewRegionLatitudeDeltaKey"
+    static let MapViewRegionLongitudeDeltaKey = "MapViewRegionLongitudeDeltaKey"
+}
+
